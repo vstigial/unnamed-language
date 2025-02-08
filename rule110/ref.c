0f732363feb9394f9423b9be2035dfd0912d6b10
vstigial <136673433+vstigial@users.noreply.github.com>
2025-02-07 22:41:21 -0700

    Update ref.c
    syntax errors fixed, should compile

--- rule110/ref.c
+++ rule110/ref.c
@@ -1,29 +1,30 @@
-#include stdio.h
-#include stdlib.h
+#include <stdio.h>
+#include <stdlib.h>
 
 #define BOARD_CAP 10
 
 int main() {
-    int board = malloc(BOARD_CAP  sizeof(int));
+    int *board = malloc(BOARD_CAP * sizeof(int));
 
-    for (size_t i = 0; i  BOARD_CAP; ++i) {
+    for (size_t i = 0; i < BOARD_CAP; ++i) {
         board[i] = 0;
     }
-    
+
     board[BOARD_CAP - 2] = 1;
 
-    for (size_t i = 0; i  BOARD_CAP - 2; ++i) {
-        for (size_t j = 0; j  BOARD_CAP; ++j) {
-            fputc(board[j] == 0  ' '  '', stdout);
+    for (size_t i = 0; i < BOARD_CAP - 2; ++i) {
+        for (size_t j = 0; j < BOARD_CAP; ++j) {
+            fputc(board[j] == 0 ? ' ' : '*', stdout);
         }
-        fputc('n', stdout);
-        int pattern = (board[0]  1)  board[1];
-        for (size_t j = 1; j  BOARD_CAP - 1; ++j) {
-            pattern = ((pattern  1) & 7)  board[j + 1];
-            board[j] = (110  pattern) & 1;
+        fputc('\n', stdout);
+
+        int pattern = (board[0] << 1) | board[1];
+        for (size_t j = 1; j < BOARD_CAP - 1; ++j) {
+            pattern = ((pattern << 1) & 7) | board[j + 1];
+            board[j] = (110 >> pattern) & 1;
         }
     }
-    
+
     free(board);
     return 0;
 }
